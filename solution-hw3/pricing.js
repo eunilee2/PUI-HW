54ac7c81f9090c1947eb5e67a8e01f3651ca8025
eunilee2 <115505438+eunilee2@users.noreply.github.com>
2023-02-15 17:02:49 -0500

    hw 3 js

--- solution-hw3/pricing.js
+++ solution-hw3/pricing.js
@@ -1,8 +1,38 @@
-let bunPrice=2.49;
+let glazings=[
+    {select:'Keep original', add:0.00},
+    {select:'Sugar milk', add:0.00},
+    {select:'Vanilla milk', add:0.50},
+    {select:'Double chocolate', add:1.50}
+]
 
-function glazingChange(element){
-    glazingValue=document.querySelector('#glazingOptions');
-    priceChange=glazingValue.options[glazingValue.selectedIndex].value;
+let sizes=[
+    {size:1,multiply:1},
+    {size:3,multiply:3},
+    {size:6,multiply:5},
+    {size:12,multiply:10},
+]
+
+let glazingOptions=document.querySelector('#glazingOptions');
+let sizeOptions=document.querySelector('#sizeOptions');
+let index=0;
+
+for (index<glazings.length; index++;)
+{
+    let selected=glazings[index];
+    let option=document.createElement('option');
+    option.text=selected.select;
+    option.value=selected.add;
+    glazingOptions.add(option);
 }
 
-document.querySelector('.priceChange').content=priceChange;
+for (index<sizes.length; index++;)
+{
+    let selected=sizes
+}
+
+
+
+function onSelectValueChange(){
+    letglazingPrice=parseFloat(glazingOptions.value);
+
+}
