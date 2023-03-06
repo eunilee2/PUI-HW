7292a90af591b0861bd671a36d5fffb9a5bb3f42
eunilee2 <115505438+eunilee2@users.noreply.github.com>
2023-03-05 21:03:27 -0500

    hw4

---
+++ solution-hw4/js/pricing.js
@@ -0,0 +1,48 @@
+let glazings=[
+    {select:'Keep original', add:0.00},
+    {select:'Sugar milk', add:0.00},
+    {select:'Vanilla milk', add:0.50},
+    {select:'Double chocolate', add:1.50}
+]
+
+let sizes=[
+    {size:1,multiply:1},
+    {size:3,multiply:3},
+    {size:6,multiply:5},
+    {size:12,multiply:10},
+]
+
+let glazingOptions=document.querySelector('#glazingOptions');
+let sizeOptions=document.querySelector('#sizeOptions');
+
+for (let i=0; i<glazings.length; i++)
+{
+    let selected=glazings[i];
+    let option=document.createElement('option');
+    option.text=selected.select;
+    option.value=selected.add;
+    glazingOptions.add(option);
+}
+
+for (let i=0; i<sizes.length; i++)
+{
+    let selected=sizes[i];
+    let option=document.createElement('option');
+    option.text=selected.size;
+    option.value=selected.multiply;
+    sizeOptions.add(option);
+}
+
+glazingOptions.addEventListener('change', onSelectValueChange);
+sizeOptions.addEventListener('change', onSelectValueChange);
+
+function onSelectValueChange(){
+    let glazingPrice=parseFloat(glazingOptions.value);
+    let sizePrice=parseFloat(sizeOptions.value);
+    const bunPrice=2.49;
+    let displayPrice=document.querySelector('.price');
+    let itemPrice=(bunPrice+glazingPrice)*sizePrice;
+    let roundedPrice='$'+itemPrice.toFixed(2);
+
+    displayPrice.innerText=roundedPrice;
+}
--- solution-hw4/js/rollsData.js
+++ solution-hw4/js/rollsData.js
@@ -24,3 +24,15 @@ const rolls = {
         "imageFile": "strawberry-cinnamon-roll.jpg"
     }    
 };
+
+const cart=[];
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get(‘roll’);
+
+const headerElement=document.querySelector('#cinnamon-roll-header');
+headerElement.innerText=rollType
+
+const rollImage=document.querySelector('#roll-image');
+rollImage.src='./assets/-'+rollType+'-cinnamon-roll.jpg';
